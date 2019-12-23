36d7008c7355187752a06d66b8c59f7754988582
Jingwei Peng <jay.peng@inlight.com.au>
2019-12-23 14:01:36 +1100

    feat: store meta data in dynamodb table

--- src/upload.ts
+++ src/upload.ts
@@ -4,7 +4,10 @@ import axios from 'axios';
 
 import getKey from './lib/get-key';
 
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', convertEmptyValues: true });
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+const META_DATA_TABLE = process.env.META_DATA_TABLE_NAME!;
+const FAILED_RECORDS_TABLE_NAME = process.env.FAILED_RECORDS_TABLE_NAME!;
 
 interface Image {
   contentType: string;
@@ -25,19 +28,56 @@ const handler = async ({ Records: [record] }) => {
   console.log(`Handling ${_.size(urls)} images`);
 
   const promises = _.map(urls, async url => {
-    const image = await getImage(url);
-    const key = getKey(url);
-    if (key) {
-      await s3
-        .upload({
-          Body: image.data,
-          Bucket: process.env.S3_BUCKET_NAME!,
-          ContentType: image.contentType,
-          Key: key,
+    let item = {
+      cloudinary_url: url,
+    };
+    try {
+      const image = await getImage(url);
+      const key = getKey(url);
+      item['s3_file_key'] = key;
+      if (key) {
+        await s3
+          .upload({
+            Body: image.data,
+            Bucket: process.env.S3_BUCKET_NAME!,
+            ContentType: image.contentType,
+            Key: key,
+          })
+          .promise();
+        await db
+          .put({
+            TableName: META_DATA_TABLE,
+            Item: { ...item, succeed: true },
+          })
+          .promise();
+      } else {
+        console.log('Unable to handle file', url);
+        await db
+          .put({
+            TableName: META_DATA_TABLE,
+            Item: { ...item, succeed: false },
+          })
+          .promise();
+        await db
+          .put({
+            TableName: FAILED_RECORDS_TABLE_NAME,
+            Item: { ...item, reason: 'Could not generate file key for this url' },
+          })
+          .promise();
+      }
+    } catch (error) {
+      await db
+        .put({
+          TableName: META_DATA_TABLE,
+          Item: { ...item, succeed: false, reason: _.get(error, 'message') },
         })
         .promise();
-    } else {
-      console.log('Unable to handle file', url);
+      await db
+        .put({
+          TableName: FAILED_RECORDS_TABLE_NAME,
+          Item: { ...item, reason: _.get(error, 'message') },
+        })
+        .promise();
     }
   });
 
